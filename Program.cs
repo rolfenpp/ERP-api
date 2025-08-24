c7e88a28237cc9a4080095ea18f2c2157c921119
rolfenpp <dala.isakk@gmail.com>
2025-08-24 14:22:26 +0200

    test

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ namespace Recipt_api
                     policy.WithOrigins(allowedOrigins)
                           .AllowAnyHeader()
                           .AllowAnyMethod();
-                    // .AllowCredentials() ta kakan sen
+                    // .AllowCredentials() ta kakan senare
                 });
             });
 
